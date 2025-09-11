5f3cc692411473e5b39bbfc4e083d7fffbd0b375
sasilva1998 <sasilva1998@gmail.com>
2025-09-11 22:07:21 +0100

    chore: node exec config

--- dr_spaam_ros/setup.py
+++ dr_spaam_ros/setup.py
@@ -1,25 +1,26 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'dr_spaam_ros'
+package_name = "dr_spaam_ros"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share/", package_name), glob("launch/*.launch.py")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='sasm',
-    maintainer_email='sasilva1998@gmail.com',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="sasm",
+    maintainer_email="sasilva1998@gmail.com",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": ["dr_spaam_ros_node = dr_spaam_ros.dr_spaam_ros:main"],
     },
 )
