7e71a582a33a364c1b5ce25c4e1ac433b899dd26
sasilva1998 <sasilva1998@gmail.com>
2025-10-04 16:06:11 -0500

    chore: configs

--- dr_spaam_ros/setup.py
+++ dr_spaam_ros/setup.py
@@ -11,7 +11,15 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share/", package_name), glob("launch/*.launch.py")),
+        # added launch and config files for execution from ROS2
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.py")),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.yaml")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
