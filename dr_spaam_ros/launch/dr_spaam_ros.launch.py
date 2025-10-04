c34995a6766a79f7cb79eb98f8d7fec1dc7d9a2f
sasilva1998 <sasilva1998@gmail.com>
2025-10-04 16:06:29 -0500

    chore: launch params

--- dr_spaam_ros/launch/dr_spaam_ros.launch.py
+++ dr_spaam_ros/launch/dr_spaam_ros.launch.py
@@ -1,20 +1,26 @@
 from launch_ros.actions import Node
 from launch import LaunchDescription
 
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
     """Contains all of the nodes to be launched"""
 
+    dr_spaam_ros_dir = FindPackageShare(package="dr_spaam_ros").find("dr_spaam_ros")
+
     # ===========================================
     # ! NODES
     # ===========================================
-    dr_spaam_ros_node = (
-        Node(
-            package="dr_spaam_ros",
-            executable="dr_spaam_ros_node",
-            name="dr_spaam_ros_node",
-            output="screen",
-        ),
+    dr_spaam_ros_node = Node(
+        package="dr_spaam_ros",
+        executable="dr_spaam_ros_node",
+        name="dr_spaam_ros_node",
+        output="screen",
+        parameters=[
+            dr_spaam_ros_dir + "/config/dr_spaam_ros.yaml",
+            dr_spaam_ros_dir + "/config/topics.yaml",
+        ],
     )
 
     # ===========================================
